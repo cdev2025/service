2ad753245b8540ee58e08e861cd6f409017f124f
cdev2025 <creativedev2025@gmail.com>
2025-11-28 09:55:41 +0900

    [2025-11-28] WebConfig : CORS 설정 추가

--- api/src/main/java/com/delivery/api/config/web/WebConfig.java
+++ api/src/main/java/com/delivery/api/config/web/WebConfig.java
@@ -6,6 +6,7 @@ import com.delivery.api.common.resolver.UserSessionResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -51,5 +52,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userSessionResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // CORS 요청에 인증 포함 허용.쿠키.세션.Authorization헤더
+
     }
 }
